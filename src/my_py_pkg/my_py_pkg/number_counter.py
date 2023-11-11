834c10ed154177a6ab2b27f8059d7a516df9c6ea
Taha Eren Karakış <jpantherr@gmail.com>
2023-11-11 15:00:39 +0300

    Number counter service eklendi.

--- src/my_py_pkg/my_py_pkg/number_counter.py
+++ src/my_py_pkg/my_py_pkg/number_counter.py
@@ -4,6 +4,7 @@ import rclpy
 from rclpy.node import Node
 
 from example_interfaces.msg import Int64
+from example_interfaces.srv import SetBool
 
 class NumberCounterNode(Node):
 
@@ -13,6 +14,7 @@ class NumberCounterNode(Node):
 
         self.number_publisher_ = self.create_publisher(Int64, "number_count", 10)
         self.subscriber_ = self.create_subscription(Int64, "number", self.callback_number_count, 10)
+        self.reset_counter_service_ = self.create_service(SetBool, "reset_counter", self.callback_reset_counter)
 
         self.get_logger().info("Number counter has been started.")
 
@@ -23,6 +25,15 @@ class NumberCounterNode(Node):
         new_msg.data = self.counter_
         self.number_publisher_.publish(new_msg)
 
+    def callback_reset_counter(self, request, response):
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter has been reset"
+        else:
+            response.success = False
+            response.message = "Counter has not been reset"
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
