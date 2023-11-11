6a07584017e93b38704ccf9df73b5919a13a2c80
Taha Eren Karakış <jpantherr@gmail.com>
2023-11-11 13:47:06 +0300

    number publisher eklendi.

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -25,7 +25,8 @@ setup(
             "satellite = my_py_pkg.satellite:main",
             "add_two_ints_server = my_py_pkg.add_two_ints_server:main",
             "add_two_ints_client_no_oop = my_py_pkg.add_two_ints_client_no_oop:main",
-            "add_two_ints_client_oop = my_py_pkg.add_two_ints_client_oop:main"
+            "add_two_ints_client_oop = my_py_pkg.add_two_ints_client_oop:main",
+            "number_publisher = my_py_pkg.number_publisher:main"
         ],
     },
 )
